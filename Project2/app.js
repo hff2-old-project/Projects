2eb5e29da14f1b8fbcf6df4be7585ea9200d97cc
hff2 <hophoebeho@gmail.com>
2020-12-19 22:37:32 +0800

    fixing small Js issues

--- Project2/app.js
+++ Project2/app.js
@@ -1,6 +1,6 @@
 const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
-const links = nav.querySelector("a");
+const links = nav.querySelectorAll("a");
 
 burger.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
